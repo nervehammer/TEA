d178b10140bec91037517dab50421e4debb02503
nervehammer <nervehammer.suraj@gmail.com>
2017-03-09 18:48:39 +0530

    Implemented File picker

--- app/src/main/java/com/procleaus/tea/EncryptActivity.java
+++ app/src/main/java/com/procleaus/tea/EncryptActivity.java
@@ -1,6 +1,7 @@
 package com.procleaus.tea;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -22,20 +23,31 @@ import java.util.Calendar;
 
 public class EncryptActivity extends AppCompatActivity {
 
-    Button btnDatePicker, btnTimePicker,btneit;
+    Button btnDatePicker, btnTimePicker, btneit, btnatt;
     EditText txtDate, txtTime;
     private int mYear, mMonth, mDay, mHour, mMinute;
+    private static final int PICKFILE_RESULT_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encrypt);
 
-        btnDatePicker=(Button)findViewById(R.id.btn_date);
-        btnTimePicker=(Button)findViewById(R.id.btn_time);
-        txtDate=(EditText)findViewById(R.id.in_date);
-        txtTime=(EditText)findViewById(R.id.in_time);
-        btneit=(Button)findViewById(R.id.btn_eit);
+        btnDatePicker = (Button) findViewById(R.id.btn_date);
+        btnTimePicker = (Button) findViewById(R.id.btn_time);
+        btneit = (Button) findViewById(R.id.btn_eit);
+        btnatt = (Button) findViewById(R.id.btnatt);
+        txtDate = (EditText) findViewById(R.id.in_date);
+        txtTime = (EditText) findViewById(R.id.in_time);
+
+        btnatt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("file/*");
+                startActivityForResult(intent, PICKFILE_RESULT_CODE);
+            }
+        });
 
         btnDatePicker.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,16 +59,16 @@ public class EncryptActivity extends AppCompatActivity {
                 mDay = c.get(Calendar.DAY_OF_MONTH);
 
 
-                DatePickerDialog datePickerDialog = new DatePickerDialog(EncryptActivity.this,new DatePickerDialog.OnDateSetListener() {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(EncryptActivity.this, new DatePickerDialog.OnDateSetListener() {
 
-                            @Override
-                            public void onDateSet(DatePicker view, int year,
-                                                  int monthOfYear, int dayOfMonth) {
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
 
-                                txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                        txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
 
-                            }
-                        }, mYear, mMonth, mDay);
+                    }
+                }, mYear, mMonth, mDay);
                 datePickerDialog.show();
 
             }
@@ -70,15 +82,15 @@ public class EncryptActivity extends AppCompatActivity {
                 mMinute = c.get(Calendar.MINUTE);
 
                 // Launch Time Picker Dialog
-                TimePickerDialog timePickerDialog = new TimePickerDialog(EncryptActivity.this,new TimePickerDialog.OnTimeSetListener() {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(EncryptActivity.this, new TimePickerDialog.OnTimeSetListener() {
 
-                            @Override
-                            public void onTimeSet(TimePicker view, int hourOfDay,
-                                                  int minute) {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay,
+                                          int minute) {
 
-                                txtTime.setText(hourOfDay + ":" + minute);
-                            }
-                        }, mHour, mMinute, false);
+                        txtTime.setText(hourOfDay + ":" + minute);
+                    }
+                }, mHour, mMinute, false);
                 timePickerDialog.show();
             }
         });
@@ -91,5 +103,16 @@ public class EncryptActivity extends AppCompatActivity {
         });
 
     }
-}
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case PICKFILE_RESULT_CODE:
+                if (resultCode == RESULT_OK) {
+                    //TODO : Implement file fetch from storage and send for encryption.
+                    Toast.makeText(EncryptActivity.this, "File Picked ", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+}
