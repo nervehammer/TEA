d76341e69001680dab9d70bb39cc2938bf498cfe
nervehammer <nervehammer.suraj@gmail.com>
2017-03-09 14:00:14 +0530

    Some function implemented with proper structure initialization

--- app/src/main/java/com/procleaus/tea/DecryptActivity.java
+++ app/src/main/java/com/procleaus/tea/DecryptActivity.java
@@ -1,8 +1,18 @@
 package com.procleaus.tea;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
 /**
  * Created by suraj on 07-03-2017.
  */
 
-public class DecryptActivity {
+public class DecryptActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(getApplicationContext(),"Decryption of your data takes place here",Toast.LENGTH_LONG).show();
+    }
 }
--- app/src/main/java/com/procleaus/tea/MainActivity.java
+++ app/src/main/java/com/procleaus/tea/MainActivity.java
@@ -1,7 +1,13 @@
 package com.procleaus.tea;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        button btn1=(button)findViewById(R.id.btn_e);
+//        button btn2= (button)findViewById(R.id.btn_d);
+
+    }
+
+    public void btneonclick(View v) {
+        Intent i = new Intent(MainActivity.this, EncryptActivity.class);
+        startActivity(i);
+
+    }
+
+    public void btndoclick(View v) {
+        Intent i = new Intent(MainActivity.this, DecryptActivity.class);
+        startActivity(i);
+    }
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 }
+
+
