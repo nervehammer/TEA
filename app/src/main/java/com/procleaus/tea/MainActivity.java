4b94d376fc97a05d4c39013662400733973277f3
nervehammer <nervehammer.suraj@gmail.com>
2017-04-09 19:18:06 +0530

    Revert "Internal Sdcard TEA/Encrypted Path Added"

--- app/src/main/java/com/procleaus/tea/MainActivity.java
+++ app/src/main/java/com/procleaus/tea/MainActivity.java
@@ -1,20 +1,15 @@
 package com.procleaus.tea;
 
 import android.content.Intent;
-import android.os.Environment;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
-
-import java.io.File;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
